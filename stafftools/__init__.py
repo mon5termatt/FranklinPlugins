56de2a0a03fabb85f585c69a5f4c26a25fba90ae
Matthew Clark <43628254+mon5termatt@users.noreply.github.com>
2019-07-28 16:53:03 -0500

    Delete __init__.py

--- stafftools/__init__.py
+++
@@ -1,9 +0,0 @@
-"""Package for stafftools cog."""
-from .stafftools import stafftools
-
-
-async def setup(bot):
-    """Load stafftools cog."""
-    cog = stafftools(bot)
-    await cog.initialize()
-    bot.add_cog(cog)
